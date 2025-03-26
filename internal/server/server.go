3bd6c86373afba485eb2b37a69f21d49ac1e7009
anton <antonfedorov190@yandex.ru>
2025-03-27 00:02:12 +0300

    fix(server): little fix

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,13 +2,11 @@ package server
 
 import (
 	"context"
-	"errors"
 	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-
 	"golang.org/x/sync/errgroup"
 )
 
@@ -29,41 +27,53 @@ func NewShortenerServer(addr string, handler http.Handler, logger *slog.Logger) 
 }
 
 func (ss *ShortenerServer) ListenServe() error {
-
 	ctxGroup, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	errGroup, ctx := errgroup.WithContext(ctxGroup)
-	stopCh := make(chan os.Signal, 1)
 
+	stopCh := make(chan os.Signal, 1)
 	signal.Notify(stopCh, syscall.SIGTERM, syscall.SIGINT)
 
 	errGroup.Go(func() error {
 		ss.log.Info("Starting server", slog.String("addr", ss.server.Addr))
-		if err := ss.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			ss.log.Error("ListenAndServe error", slog.String("error", err.Error()))
-			return err
+		
+		errCh := make(chan error, 1)
+
+		go func() {
+			errCh <- ss.server.ListenAndServe()
+		}()
+
+		select {
+		case <-ctx.Done():
+			ss.log.Info("Context canceled, shutting down server...")
+		case err := <-errCh:
+			if err != nil && err != http.ErrServerClosed {
+				ss.log.Error("Server error", slog.String("error", err.Error()))
+				return err
+			}
 		}
 		return nil
 	})
 
 	errGroup.Go(func() error {
-		ss.log.Info("Selector starting...")
 		select {
 		case <-stopCh:
 			ss.log.Info("Received shutdown signal")
+			cancel() 
 			return nil
 		case <-ctx.Done():
 			return ctx.Err()
 		}
 	})
 
+	// Ожидаем завершения всех горутин
 	if err := errGroup.Wait(); err != nil {
 		ss.log.Error("Server encountered an error", slog.String("error", err.Error()))
 		return err
 	}
 
-
+	// Завершаем работу сервера
 	if err := ss.ShutDown(context.Background()); err != nil {
 		ss.log.Error("Server shutdown failed", slog.String("error", err.Error()))
 	}
@@ -72,6 +82,7 @@ func (ss *ShortenerServer) ListenServe() error {
 
 	return nil
 }
+
 
 
 func (ss *ShortenerServer) ShutDown(ctx context.Context) error {
